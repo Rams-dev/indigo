499f25cad382e6c97fcc90fe2133408b2b4dcc0a
RamiroSW <desarrollo.sw@grupobonn.com>
2024-11-22 09:33:42 -0600

    cambios

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -85,24 +85,26 @@ export class CalendarComponent implements OnInit, AfterViewInit{
       datesSet: (info) => {
         this.dateStart = moment(info.view.currentStart).format('yyyy-MM-DD HH:mm:ss')
         this.dateEnd = moment(info.view.currentEnd).format('yyyy-MM-DD HH:mm:ss')
-        this.getEvents()
-        this.getHolidays()
-        this.crearHorario()
+        this.init()
         
       }
 
     }
   }
 
+
+  init(){
+    this.getEvents()
+    this.getHolidays()
+    this.crearHorario()
+    this.getEventsWithOutDate()
+
+  }
+
   ngOnInit(): void {
     this.dateStart = moment(new Date()).format('YYYY-MM-01')
     this.dateEnd = moment(new Date()).add(1, 'M').format('YYYY-MM-01')
-
-    this.getEvents()
-    this.getHolidays()
-    this.getEventsWithOutDate()
-    this.crearHorario()
-    
+    this.init()
   }
 
 
@@ -128,9 +130,7 @@ export class CalendarComponent implements OnInit, AfterViewInit{
       "dateEnd": moment(this.dateEnd).format("YYYY-MM-DD")
     }
     this.holidayService.getByParams(obj).
-    pipe(map(response => {;
-      
-      
+    pipe(map(response => {
       return response.data.map((res:any) => {
         this.holidays.push(res.date)
         
@@ -145,23 +145,18 @@ export class CalendarComponent implements OnInit, AfterViewInit{
       })
     })).
     subscribe(res => {
-      
-
-      this.events = [...this.events, ...res]
-
-      console.log("hokiday");
       console.log(res);
-      console.log("events");
-      console.log(this.events);
+      
+      this.events = [...this.events, ...res]
       this.calendarOptions.events = this.events
     })
-
-
   }
 
 
   getEventsWithOutDate(){
     this.eventService.getByParams({"date":"no"}).subscribe(res => {
+      console.log(res);
+      
       this.eventsWithOutDate = this.formatEvents(res.data)
     })
 
@@ -176,32 +171,25 @@ export class CalendarComponent implements OnInit, AfterViewInit{
     }
     this.eventService.getByParams(obj).
     subscribe(data => {
-      // this.eventsCalendar = this.formatEvents(data)
       this.events = this.formatEvents(data.data)
+      console.log(data.data);  
+      
       this.calendarOptions.events = this.events
-      // this.calendarOptions.events = this.formatEvents(data.data)
       
     })
   }
 
   eventClick(event:any){
-    console.log("eventClick");
-    console.log(event);
-    
-
-    this.openModal(event.event.extendedProps, true)
-    
-    // this.update(event.event.extendedProps)
-    
-
+    this.openModal(event.event.extendedProps, true)  
   }
 
   formatEvents(data:any){
-    return data.map((res:any) => {
 
+    return data.map((res:any) => {
+      let hour = moment(res.dateStart).format("HH:MM:SS")
+      console.log(hour);
       let allDay = moment(res.dateStart).format("HH:MM:SS").startsWith("00:")
-      
-      
+
       return {
         "idEvent":res.idEvent,
         "title":res.title,
@@ -217,15 +205,7 @@ export class CalendarComponent implements OnInit, AfterViewInit{
   }
 
   update(event:any){
-    // console.log(event.event.start);
-    // console.log(event.event.end);
-    
-    // let data = event.event.extendedProps
-    // data.dateStart = moment(event.event.start).format("YYYY-MM-DD")
-    // console.log(data)
     this.eventService.put(event.idEvent, event).subscribe(data => {
-      
-
     })
   }
 
@@ -233,8 +213,6 @@ export class CalendarComponent implements OnInit, AfterViewInit{
   eventReceive(event:any){
 
     let receiveDate = moment(event.event.start).format("YYYY-MM-DD");
-    
-    
     
     if(this.isHoliDay(receiveDate)){
       event.revert()
@@ -255,7 +233,6 @@ export class CalendarComponent implements OnInit, AfterViewInit{
 
   parseFecha(date:any){
     return moment(date).format()
-
   }
 
 
@@ -284,31 +261,27 @@ export class CalendarComponent implements OnInit, AfterViewInit{
   }
 
   dateClick(event:any){    
+    if(this.isHoliDay(moment(event.dateStr).format("YYYY-MM-DD"))) {
+      console.log("isHoliday"); 
+      return
+    }
     
-    
-    
-    if(this.isHoliDay(moment(event.dateStr).format("YYYY-MM-DD"))) return
     this.openModal(event)
   }
 
   openModal(data:any, mostrarinfo = false){
     
-    
-    console.log(data);
-    
     const dialogRef = this.dialog.open(ModalEventComponent, {height:'auto', width:'50%', data:{data, mostrarinfo}, })
     .afterClosed().subscribe(data => {
       if(data == 'creado'){
-        this.getEvents()
-        this.getEventsWithOutDate()
+        this.init()
       }
     });
   }
 
 
-  isHoliDay(date:any){
+  isHoliDay(date:any){   
     return this.holidays.includes(date)
-
   }
 
 }
--- src/app/pages/calendar/modal-event/modal-event.component.ts
+++ src/app/pages/calendar/modal-event/modal-event.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { EventService } from '../../events/event.service';
 import { Dialog } from '@angular/cdk/dialog';
+import moment from 'moment';
 
 @Component({
   selector: 'app-modal-event',
@@ -21,6 +22,7 @@ export class ModalEventComponent{
   readonly dialogRef = inject(MatDialogRef<ModalEventComponent>);
   data = inject<any>(MAT_DIALOG_DATA);
   mostrarInfo:boolean = false
+  esFestivo :boolean = false
   info:any 
 
   form:FormGroup
@@ -38,6 +40,7 @@ export class ModalEventComponent{
 
   ngOnInit(): void {
     console.log(this.data);
+    this.esFestivo = this.data.data.idHoliday ? true : false
     
 
     if(this.data.mostrarinfo){
@@ -45,33 +48,42 @@ export class ModalEventComponent{
       this.info = this.data.data
 
     }
-
-      
       
     this.form = new FormGroup({
-      idEvent: new FormControl(''),
+      idEvent: new FormControl(null),
       title: new FormControl('', Validators.required),
       description : new FormControl('', Validators.required),
-      dateStart : new FormControl(''),
-      dateEnd : new FormControl(''),
+      dateStart : new FormControl(null),
+      dateEnd : new FormControl(null),
     })
   
     this.form.markAllAsTouched(); 
     this.form.patchValue(this.data.data)
-    this.form.patchValue({dateStart: this.data.data.dateStr})
+    if (this.data.data.dateStr){
+      this.form.patchValue({dateStart: moment(this.data.data.dateStr).format("YYYY-MM-DD HH:mm:ss")})
+
+    }
+
+    console.log(this.form.value);
+    
 
     
   }
 
   
   save(){
+
+    // console.log(this.form.value);
+    // return
+    
     this.eventService.post(this.form.value).subscribe(data => {
-      
+      console.log(data);
       this.dialogRef.close("creado")
       
     })
     
   }
+
 
 
   actualizar(){
